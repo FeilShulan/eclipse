717934c684c3bb7b4c0397d9fd70d7e038b529a4
FeilShulan <37323454+FeilShulan@users.noreply.github.com>
2018-09-27 17:37:36 +0800

    发布项目

---
+++ com.shulan/src/main/java/mygit/com/shulan/App.java
@@ -0,0 +1,13 @@
+package mygit.com.shulan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
