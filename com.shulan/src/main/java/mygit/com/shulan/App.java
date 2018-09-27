1bbe1cb8ccf6c530c40c6224c44baa81e24090f6
FeilShulan <37323454+FeilShulan@users.noreply.github.com>
2018-09-27 18:03:30 +0800

    Update App.java

--- com.shulan/src/main/java/mygit/com/shulan/App.java
+++ com.shulan/src/main/java/mygit/com/shulan/App.java
@@ -10,4 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    public static void main2( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
